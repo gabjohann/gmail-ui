f4d1620c1e388896552b55c89a0d128b63b5cf3e
Lucas Gabriel <gabif.johann@gmail.com>
2024-02-18 18:47:43 -0300

    home screen ui

---
+++ src/components/avatar.tsx
@@ -0,0 +1,19 @@
+import { Image, ImageProps } from 'react-native'
+import clsx from 'clsx'
+
+type AvatarProps = ImageProps & {
+  size?: 'small' | 'medium'
+}
+
+export function Avatar({ size = 'medium', ...rest }: AvatarProps) {
+  return (
+    <Image
+      className={clsx('rounded-full', {
+        'h-8 w-8': size === 'small',
+        'h-10 w-10': size === 'medium',
+      })}
+      alt=""
+      {...rest}
+    />
+  )
+}
---
+++ src/components/email.tsx
@@ -0,0 +1,62 @@
+import { Text, View } from 'react-native'
+
+import { Avatar } from './avatar'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { EmailDataProps } from '@/utils/emails'
+
+import { colors } from '@/styles/colors'
+
+type EmailProps = {
+  data: EmailDataProps
+}
+
+export function Email({ data }: EmailProps) {
+  return (
+    <View className="w-full flex-row gap-4">
+      <Avatar source={{ uri: data.avatar }} />
+
+      <View className="flex-1">
+        <View className="flex-row items-center gap-1">
+          {data.marker && (
+            <MaterialIcons
+              name="label-important"
+              size={16}
+              color={colors.yellow[600]}
+            />
+          )}
+
+          <Text className="flex-1 font-subtitle text-lg text-gray-400">
+            {data.name}
+          </Text>
+
+          <Text className="font-body text-sm text-gray-400">{data.date}</Text>
+        </View>
+
+        <Text
+          className="font-body text-base text-gray-400"
+          numberOfLines={1}
+          lineBreakMode="tail"
+        >
+          {data.subject}
+        </Text>
+
+        <View className="flex-row items-center gap-4">
+          <Text
+            className="flex-1 font-body text-base text-gray-400"
+            numberOfLines={1}
+            lineBreakMode="tail"
+          >
+            {data.message}
+          </Text>
+
+          <MaterialIcons
+            name={data.start ? 'star' : 'star-outline'}
+            size={22}
+            color={data.start ? colors.yellow[600] : colors.blue[600]}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
